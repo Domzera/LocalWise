90f027e64c2a35dd3ac9e48c36c091eeb1792938
LUIZ CELIO GOMES FILHO <luizceliogf@hotmail.com>
2023-05-16 18:37:07 -0300

    Controllers de Guia,Turista e 1/2 do Gerente_Local estão prontos

--- LocalWise/Controllers/AccountController.cs
+++ LocalWise/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using LocalWise.ViewModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LocalWise.Controllers
 {
@@ -83,12 +84,14 @@ namespace LocalWise.Controllers
         //Tem que ter autorização para seguir!
         [Authorize]
 
-        //Controles do Turista
+        //===> Controles do Turista <===
+        //Tela de Registro do Turista
         public IActionResult TuristaRegister()
         {
             var response = new RegisterTuristaViewModel();
             return View(response);
         }
+        //Tela de Registro peenchida do Turista
         [HttpPost]
         public async Task<IActionResult> TuristaRegister(RegisterTuristaViewModel registerTuristaViewModel)
         {
@@ -117,13 +120,39 @@ namespace LocalWise.Controllers
 
             return RedirectToAction("Login","Account");
         }
+        //Tela de Edição do Turista
+        public IActionResult TuristaEdit()
+        {
+            var response = new TuristaEditViewModel();
+            return View(response);
+        }
+        //Tela de Edição do Turista preenchido
+        public async Task<IActionResult> TuristaEdit(string id,TuristaEditViewModel TEViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Fail to edit");
+                return View("TuristaEdit", TEViewModel);
+            }
+            //------Daqui para baixo
+            var turista = await _userManager.FindByIdAsync(id);
+            if (turista == null) return View("Error");
+            var ETViewModel = new RegisterTuristaViewModel
+            {
+                Nome = turista.UserName,
+                Email = turista.Email
+            };
+            return View(ETViewModel);
+        }
 
-        //Controles do Guia
+        //===> Controles do Guia <===
+        //Tela de Edição do Guia
         public IActionResult GuiaEdit()
         {
             var resonse = new GuiaEditViewModel();
             return View(resonse);
         }
+        //Tela de Edição do Guia preenchida
         [HttpPost]
         public async Task<IActionResult> GuiaEdit(string id,GuiaEditViewModel GEViewModel)
         {
@@ -143,14 +172,99 @@ namespace LocalWise.Controllers
             };
             return View(EGViewModel);
         }
-
+        //Tela de Registro do Guia
         public IActionResult GuiaRegister()
         {
             var response = new RegisterGuiaViewModel();
             return View(response);
         }
+        //Tela de Registro do Guia preenchida
         [HttpPost]
         public async Task<IActionResult> GuiaRegister(RegisterGuiaViewModel registerGuiaViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerGuiaViewModel);
+            }
+            var user = await _userManager.FindByEmailAsync(registerGuiaViewModel.Email);
+            if (user == null)
+            {
+                var newUser = new Pessoa()
+                {
+                    Email = registerGuiaViewModel.Email,
+                    UserName = registerGuiaViewModel.Name,
+                    DataCadastro = DateTime.Now
+                };
+                var newUserResponse = await _userManager.CreateAsync(newUser, registerGuiaViewModel.Senha);
+                if (newUserResponse.Succeeded)
+                {
+                    //Criar Função aqui para preenchimento da Role!
+                    var role = await _roleManager.RoleExistsAsync(UserRoles.Guia);
+                    if (!role) await _roleManager.CreateAsync(new IdentityRole(UserRoles.Guia));
+                    await _userManager.AddToRoleAsync(newUser, UserRoles.Guia);
+                }
+                var foto = await _photoService.AddPhotoAsync(registerGuiaViewModel.UrlPhoto);
+                var guia = new Guia()
+                {
+                    UrlPhoto = registerGuiaViewModel.UrlPhoto.ToString()
+                };
+                return RedirectToAction("index", "Guia");
+            }
+
+            return RedirectToAction("Login", "Account");
+        }
+
+        //===> Controles do Gerente_Local <===
+        //Tela de Edição do Gerente_Local
+        public IActionResult GerenteLocalEdit()
+        {
+            var resonse = new GerenteLocalEditViewModel();
+            return View(resonse);
+        }
+        //Tela de Edição do Gerente_Local preenchida
+        [HttpPost, ActionName("GerenteLocalEdit")]
+        public async Task<IActionResult> GerenteLocalEdit(int id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            //-----Daqui para baixo
+            
+            var gLocal = await _context.GerenteLocals.FirstOrDefaultAsync(g => g.Id == id);
+            if (await TryUpdateModelAsync<GerenteLocal>(gLocal, "",
+                g => g.RazaoSocial,
+                g => g.Endereco.Logradouro,
+                g => g.Endereco.Numero,
+                g => g.Endereco.Bairro,
+                g => g.Endereco.Cidade,
+                g => g.Endereco.Cep,
+                g => g.Endereco.Estado
+                ))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("index", "GerenteLocal");
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Unable to save changes!");
+                }
+            }
+                return RedirectToAction("Index", "GerenteLocal");
+        }
+
+        //Fiz até aqui********************************************************************************************
+        //Tela de Registro do Gerente_Local
+        public IActionResult GerenteLocalRegister()
+        {
+            var response = new RegisterGuiaViewModel();
+            return View(response);
+        }
+        //Tela de Registro do Gerente_Local preenchida
+        [HttpPost]
+        public async Task<IActionResult> GerenteLocalRegister(RegisterGuiaViewModel registerGuiaViewModel)
         {
             if (!ModelState.IsValid)
             {
